b2c668c2cd46b56620865f48d2879887a2a93935
Sangna <rosesp.3157@gmail.com>
2025-09-23 23:34:17 +0530

    added all the functionalities in script.js along with changes in .html file

--- library/library-script.js
+++ library/library-script.js
@@ -12,15 +12,21 @@ function Book(title,author,pages,read){
         console.log(this.title+" by "+this.author+","+this.pages+"pages"+","+read);
     }
 }
+//Book Prototype
+Book.prototype.toggleRead=function(){
+    this.read=!this.read;
+}
+
 function addBookToLibrary(book){
     myLibrary.push(book)
 }
 const container=document.querySelector('#container');
-const book=new Book('Hobbit','J.R.R Tolkien',295,'not read yet');
-addBookToLibrary(book);
+
+
 // console.log(myLibrary);
 // console.log(myLibrary[0].info())
 function displayBooks(myLibrary){
+    container.innerHTML=''
     for(let i=0;i<myLibrary.length;i++){
         let card=document.createElement('div');
        let title=document.createElement('h2');
@@ -33,17 +39,39 @@ function displayBooks(myLibrary){
         let markBtn=document.createElement('button');
         let removeBtn=document.createElement('button');
         removeBtn.textContent="Remove";
-        markBtn.textContent="Mark as Read";
-        card.append(title,author,pages,removeBtn,markBtn);
-        container.append(card);
-        card.dataset.bookId= myLibrary[i].id
+        const currentBook=myLibrary[i];
+        if(currentBook.read){
+            markBtn.textContent="Mark as Unread";
+        }
+        else{
+            markBtn.textContent="Mark as Read";
 
+        }
+        card.dataset.bookId= myLibrary[i].id;
+
+        removeBtn.addEventListener('click',()=>{
+            const bookId=card.dataset.bookId;
+            const bookIndex=myLibrary.findIndex(book=>book.id=== bookId);
+            myLibrary.splice(bookIndex,1);
+            displayBooks(myLibrary);
+               
+    })
+
+    markBtn.addEventListener('click',()=>{
+        const bookId=card.dataset.bookId;
+        const bookIndex=myLibrary.findIndex(book=> book.id===bookId);
+        const book=myLibrary[bookIndex]
+        book.toggleRead();
+        displayBooks(myLibrary);
+    })
+    card.append(title,author,pages,removeBtn,markBtn);
+     container.append(card);
     }
 }
 const dialog=document.querySelector('#book-dialog');
 const newBtn =document.querySelector('#new-book');
 newBtn.addEventListener('click',()=>{
-    dialog.showModal()
+    dialog.showModal();
 });
 
 const closeBtn=document.querySelector('#close-dialog');
@@ -53,8 +81,20 @@ closeBtn.addEventListener('click',()=>{
 const form=document.querySelector('#add-book-form');
 form.addEventListener('submit',(event)=>{
     event.preventDefault();
-    console.log('Form Submitted!');
+    const titleInput=document.querySelector('#title');
+    const authorInput=document.querySelector('#author');
+    const pageInput=document.querySelector('#pages');
+    const readStatusInput=document.querySelector('input[name="readStatus"]:checked');
+    const title=titleInput.value;
+    const author=authorInput.value;
+    const pages=pageInput.value;
+    const readStatus=readStatusInput.value ==='true';
 
+    const newBook= new Book(title,author,pages,readStatus);
+    addBookToLibrary(newBook);
+    dialog.close();
+    form.reset();
+    displayBooks(myLibrary);
 })
 
 
